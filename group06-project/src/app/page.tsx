a2d83891c51715459fdfeb14591cc07879d9e7ec
Fernando Gonzalez <fernandogonzalezvaldz@gmail.com>
2024-01-25 23:05:29 -0300

    Next app crated; Lets Work!!

---
+++ group06-project/src/app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Handcrafted Haven</h1>
+      <div>
+      <p>If you are a seller</p>
+        <button>
+          Sign in
+        </button>
+      </div>
+      <div>
+        <p>Or if you are customer</p>
+        <button>
+          Click here!
+        </button>
+      </div>
+    </main>
+  )
+}
